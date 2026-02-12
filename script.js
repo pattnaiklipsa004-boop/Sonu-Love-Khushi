4c457e231adbb318f8f18337dc0d7cdfc36248b6
pattnaiklipsa004-boop <pattnaiklipsa004@gmail.com>
2026-02-12 23:13:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
-function checkPassword() {
-  var pass = document.getElementById("password").value;
-  if(pass === "ilovesonu") {
-    document.getElementById("lockScreen").style.display = "none";
-    document.getElementById("mainContent").style.display = "block";
-    startEmojiRain();
-  } else {
-    alert("Wrong password baby 😢");
-  }
+function checkPassword(){
+if(password.value==="iloveyou"){
+lockScreen.style.display="none";
+mainContent.style.display="block";
+heroImg.classList.add("color");
+startHearts();
+}else{
+alert("Wrong password 😢");
+}
 }
 
-function flipCard(card) {
-  card.classList.toggle("flipped");
+function flipCard(card){
+card.classList.toggle("flipped");
 }
 
-function openLetter(type) {
-  let msg = "";
-  if(type === "happy") msg = "I love seeing you smile 😊 You are my sunshine forever ☀️💖";
-  if(type === "sad") msg = "Come here, I’m hugging you tight 🤗 Everything will be okay baby 💕";
-  if(type === "miss") msg = "Even when we are apart, my heart beats only for you 💓";
-  alert(msg);
+function openLetter(){
+letterModal.style.display="block";
+letterText.innerHTML=
+"My baby sweetuuuu 💖<br><br>\
+From the moment you came into my life everything became brighter ✨\
+You are my happiness, my comfort, my future, my forever. \
+I promise to stand beside you in every storm 🌧️ and celebrate every sunshine ☀️\
+We will travel the world ✈️ build our dream home 🏡 and grow old together 👵👴\
+No matter what happens I will always choose you again and again 💍💖\
+I love you more than words can ever explain 💕";
 }
 
-function sayYes() {
-  document.getElementById("result").innerHTML =
-    "<h2>Yayyyy!! 💖😘</h2><img src='images/Kiss-panda.jpg' width='200'>";
+function closeLetter(){
+letterModal.style.display="none";
 }
 
-function sayNo() {
-  document.getElementById("result").innerHTML =
-    "<h2>How dare you 😡</h2><img src='images/angry-panda.jpg' width='200'>";
+function sayYes(){
+result.innerHTML="<h2>I KNEW ITTTT 😍💖</h2><img src='images/kiss-panda.jpg' width='200'>";
 }
 
-/* EMOJI RAIN */
-function startEmojiRain() {
-  setInterval(() => {
-    const emoji = document.createElement("div");
-    emoji.classList.add("emoji");
-    emoji.innerHTML = "💖";
-    emoji.style.left = Math.random() * 100 + "vw";
-    emoji.style.animationDuration = (Math.random() * 3 + 2) + "s";
-    document.body.appendChild(emoji);
-    setTimeout(() => emoji.remove(), 5000);
-  }, 300);
+function sayNo(){
+result.innerHTML="<h2>How dare youuu 😡</h2><img src='images/angry-panda.jpg' width='200'>";
+}
+
+function startHearts(){
+setInterval(()=>{
+let heart=document.createElement("div");
+heart.className="heart";
+heart.innerHTML="💖";
+heart.style.left=Math.random()*100+"vw";
+heart.style.animationDuration=(Math.random()*3+2)+"s";
+document.body.appendChild(heart);
+setTimeout(()=>heart.remove(),5000);
+},300);
+}
+
+let video=document.getElementById("video");
+
+function startCamera(){
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream=>video.srcObject=stream);
+}
+
+function takePhoto(){
+canvas.getContext("2d").drawImage(video,0,0,250,200);
+alert("I LOVE YOUUUU 💖💖💖");
 }
