5489fe9dd8a007f13460d1b2490247df70535eac
pattnaiklipsa004-boop <pattnaiklipsa004@gmail.com>
2026-02-12 22:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-function checkPassword(){
-    var p=document.getElementById("password").value;
-    if(p==="ilovesonu"){
-        document.getElementById("lockScreen").style.display="none";
-        document.getElementById("mainContent").style.display="block";
-    } else {
-        alert("Wrong password baby 😭");
-    }
+function checkPassword() {
+  var pass = document.getElementById("password").value;
+  if(pass === "ilovesonu") {
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("mainContent").style.display = "block";
+    startEmojiRain();
+  } else {
+    alert("Wrong password baby 😢");
+  }
 }
 
-function flipCard(card){
-    card.classList.toggle("flip");
+function flipCard(card) {
+  card.classList.toggle("flipped");
 }
 
-function showMessage(type){
-    if(type==="happy") alert("App khush ho to meri duniya aur bright ho jaati hai 💖");
-    if(type==="sad") alert("App sad ho to main hamesha app ke saath ho baby jann 💕");
-    if(type==="miss") alert("Miss karte ho to tight hug samajh lo 🤗💘");
+function openLetter(type) {
+  let msg = "";
+  if(type === "happy") msg = "I love seeing you smile 😊 You are my sunshine forever ☀️💖";
+  if(type === "sad") msg = "Come here, I’m hugging you tight 🤗 Everything will be okay baby 💕";
+  if(type === "miss") msg = "Even when we are apart, my heart beats only for you 💓";
+  alert(msg);
 }
 
-function sayYes(){
-    document.getElementById("result").innerHTML =
-    "<h1>Yayyyy 💖</h1><img src='images/kiss-panda.jpg' width='300'><h2>Panda Kisses 😘🌸🌸🌸</h2>";
+function sayYes() {
+  document.getElementById("result").innerHTML =
+    "<h2>Yayyyy!! 💖😘</h2><img src='images/Kiss-panda.jpg' width='200'>";
 }
 
-function sayNo(){
-    document.getElementById("result").innerHTML =
-    "<h1>Angry Panda 😡</h1><img src='images/angry-panda.jpg' width='300'>";
+function sayNo() {
+  document.getElementById("result").innerHTML =
+    "<h2>How dare you 😡</h2><img src='images/angry-panda.jpg' width='200'>";
+}
+
+/* EMOJI RAIN */
+function startEmojiRain() {
+  setInterval(() => {
+    const emoji = document.createElement("div");
+    emoji.classList.add("emoji");
+    emoji.innerHTML = "💖";
+    emoji.style.left = Math.random() * 100 + "vw";
+    emoji.style.animationDuration = (Math.random() * 3 + 2) + "s";
+    document.body.appendChild(emoji);
+    setTimeout(() => emoji.remove(), 5000);
+  }, 300);
 }
